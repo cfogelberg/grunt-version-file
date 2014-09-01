29117bc3888cb2e398e5aa437689a760f8549eb9
Christo Fogelberg <cgf@syntilect.com>
2014-09-01 21:35:43 +0100

    Header comment typo correction

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*
- * grunt-set-app-mode
- * https://github.com/cfogelberg/grunt-set-app-mode
+ * grunt-version-file
+ * https://github.com/cfogelberg/grunt-version-file
  *
  * Copyright (c) 2014 Christo Fogelberg
  * Licensed under the MIT license.
