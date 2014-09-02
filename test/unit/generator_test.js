766099acf8a4f1654b8467ad014639bd9f059e69
Christo Fogelberg <cgf@syntilect.com>
2014-09-02 08:17:06 +0100

    Unit test stubs for lib classes

---
+++ test/unit/generator_test.js
@@ -0,0 +1,13 @@
+/*
+ * grunt-version-file
+ * https://github.com/cfogelberg/grunt-version-file
+ *
+ * Copyright (c) 2014 Christo Fogelberg
+ * Licensed under the MIT license.
+ */
+
+ "use strict";
+
+describe("Generator", function() {
+
+});
